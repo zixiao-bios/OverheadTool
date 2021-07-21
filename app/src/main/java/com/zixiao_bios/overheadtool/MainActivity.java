a5c9c7a15504ac24c6b12ee1640ed17990c9984b
zixiao_bios <zixiao_bios@outlook.com>
2021-07-21 19:10:07 +0800

    创建用于性能测试的服务

--- app/src/main/java/com/zixiao_bios/overheadtool/MainActivity.java
+++ app/src/main/java/com/zixiao_bios/overheadtool/MainActivity.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.TargetApi;
 import android.app.AppOpsManager;
 import android.app.usage.NetworkStats;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.usage.NetworkStatsManager;
+import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private MonitorService monitorService;
+    private final String tag = "MainActivity";
+
+    private final ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder) {
+            // 服务绑定成功
+            Log.i(tag, "MonitorService绑定成功");
+            MonitorService.MonitorServiceBinder binder = (MonitorService.MonitorServiceBinder) iBinder;
+            monitorService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // 服务断开
+            Log.i(tag, "MonitorService断开");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         // 有权限
         Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+        // todo 删除
         try {
             NetworkStats networkStats = networkStatsManager.queryDetailsForUid(
                     ConnectivityManager.TYPE_WIFI,
---
+++ app/src/main/java/com/zixiao_bios/overheadtool/MonitorService.java
@@ -0,0 +1,34 @@
+package com.zixiao_bios.overheadtool;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class MonitorService extends Service {
+    // 用于Activity和service通讯
+    private final MonitorServiceBinder monitorServiceBinder = new MonitorServiceBinder();
+    public class MonitorServiceBinder extends Binder {
+        public MonitorService getService() {
+            return MonitorService.this;
+        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return monitorServiceBinder;
+    }
+
+    // 测试开始、结束时间戳
+    long startTime, endTime;
+
+    /**
+     * 对一个指定uid的程序进行指定时长的性能测试
+     * @param duration 测试时长(ms)
+     * @param uid 待测程序的uid
+     */
+    public void runTest(long duration, int uid){
+        startTime = System.currentTimeMillis();
+        endTime = startTime + duration;
+        // todo 添加测试内容
+    }
+}
