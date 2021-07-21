38a8741957ba5bd07391a4db593fcb96e819a42e
zixiao_bios <zixiao_bios@outlook.com>
2021-07-21 19:43:18 +0800

    绑定UI组件、按钮监听

--- app/src/main/java/com/zixiao_bios/overheadtool/MainActivity.java
+++ app/src/main/java/com/zixiao_bios/overheadtool/MainActivity.java
@@ -2,26 +2,20 @@ package com.zixiao_bios.overheadtool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.TargetApi;
-import android.app.AppOpsManager;
 import android.app.usage.NetworkStats;
 import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.ConnectivityManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.app.usage.NetworkStatsManager;
 import android.os.IBinder;
-import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private MonitorService monitorService;
-    private final String tag = "MainActivity";
-
+    // 绑定MonitorService
     private final ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder iBinder) {
@@ -38,10 +32,18 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private MonitorService monitorService;
+    private final String tag = "MainActivity";
+
+    // UI组件
+    private EditText uidInput, durationInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uidInput = findViewById(R.id.uidInputText);
+        durationInput = findViewById(R.id.durationInputText);
 
         NetworkStatsManager networkStatsManager = getSystemService(NetworkStatsManager.class);
 //        NetworkStatsManager networkStatsManager = (NetworkStatsManager) getApplicationContext().getSystemService(Context.NETWORK_STATS_SERVICE);
@@ -67,5 +69,10 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    // 开始测试按钮监听
+    public void clickStartTest(View view) {
+        // todo 获取输入、启动service
     }
 }
