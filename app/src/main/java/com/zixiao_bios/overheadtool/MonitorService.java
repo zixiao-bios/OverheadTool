2d82cf792d1df84b55fbe85cd44f72c3a534090f
zixiao_bios <zixiao_bios@outlook.com>
2021-07-22 20:02:35 +0800

    完成CPU和内存统计，测试完成

--- app/src/main/java/com/zixiao_bios/overheadtool/MonitorService.java
+++ app/src/main/java/com/zixiao_bios/overheadtool/MonitorService.java
@@ -35,12 +35,24 @@ public class MonitorService extends Service {
      * @param pid      待测程序的pid
      */
     public void runMonitor(long duration, int pid) {
-        startTime = System.currentTimeMillis();
-        endTime = startTime + duration;
+        // 获取uid
         int uid = CmdTool.findUidByPid(pid);
         if (uid == -1){
             return;
         }
+
+        // 获取起止时间
+        startTime = System.currentTimeMillis();
+        endTime = startTime + duration;
+
+        // CPU、内存总用量
+        double cpuTot = 0, memTot = 0;
+
+        // CPU测量次数
+        int testNum = 0;
+
+        // 每次测量的resMap
+        HashMap<String, Double> resMapEach;
 
         // 测试开始
         Log.e(tag, "-----------------------------------------\nduration=" + duration + "\nuid=" + uid);
@@ -48,17 +60,38 @@ public class MonitorService extends Service {
         // 统计开始时网络用量
         HashMap<String, Long> netstatsMapStart = CmdTool.findUidNetstats(uid);
 
+        // 测试期间
         while (System.currentTimeMillis() < endTime) {
-            // 测试期间
+            // 每5秒测一次CPU和内存
+            resMapEach = CmdTool.findPidCpuMemStats(pid);
+            if (resMapEach == null) {
+                continue;
+            }
+            cpuTot += resMapEach.get("cpu");
+            memTot += resMapEach.get("mem");
+            testNum ++;
+
+            // 睡眠，且按时退出循环
+            try {
+                if (endTime - System.currentTimeMillis() >= 5000) {
+                    // 剩余时间大于5秒，则睡5秒
+                    Thread.sleep(5000);
+                } else {
+                    // 剩余时间不足5秒，则睡到endTime
+                    Thread.sleep(endTime - System.currentTimeMillis());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         // 测试结束
         Log.e(tag, "测试结束");
 
-        // 统计结束时网络用量
+        // 结束时网络用量
         HashMap<String, Long> netstatsMapEnd = CmdTool.findUidNetstats(uid);
 
-        // 测试期间网络用量
+        // 网络总用量
         HashMap<String, Long> netstatsMapUse = CmdTool.subNetstatsMap(netstatsMapEnd, netstatsMapStart);
         if (netstatsMapUse == null) {
             Log.e(tag, "网络统计失败");
@@ -66,12 +99,14 @@ public class MonitorService extends Service {
             Log.e(tag, netstatsMapUse.toString());
         }
 
-        // test top
-        HashMap<String, Double> resMap = CmdTool.findPidCpuMemStats(pid);
-        if (resMap == null) {
-            Log.e(tag, "CPU和内存统计失败！");
+        // CPU(%)、内存(MB)平均用量
+        if (testNum == 0){
+            Log.e(tag, "CPU、内存统计失败");
         } else {
-            Log.e(tag, resMap.toString());
+            HashMap<String, Double> resMapAve = new HashMap<>();
+            resMapAve.put("cpu", cpuTot / testNum);
+            resMapAve.put("mem", memTot / testNum);
+            Log.e(tag, resMapAve.toString());
         }
 
         Log.e(tag, "-----------------------------------------------");
